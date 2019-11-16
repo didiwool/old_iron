36953b8e429d928dd9f518d1b383072c748b8c98
Katherine Huang <katherinehuang@Katherines-Air.modem>
2019-11-16 20:12:00 +1100

    try to do the register

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def login():
         password = request.form["password"]
         print(type(username), type(password))
         r = requests.post("http://127.0.0.1:5000/login?username={}&password={}".format(username, password))
+        print(r)
         if r.status_code == 200:
             token = r.json()["token"]
             print("sucesdsdfkdfs")
@@ -23,6 +24,31 @@ def login():
         else:
             return render_template('login.html')
     return render_template('login.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        usertype = int(request.form["usertype"])
+        print(usertype)
+
+
+        url = 'http://127.0.0.1:5000/register'
+        s = json.dumps({'username': username, 'password': password,'usertype': usertype})
+        r = requests.post(url, data=s)
+        print(r)
+       
+        if r.status_code == 201:
+            print("=============")
+            return render_template("home.html")
+        else:
+            print("*************")
+            return render_template('register.html')
+
+        print("-----------" + r.status_code)
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
